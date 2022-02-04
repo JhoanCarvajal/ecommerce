54c182c6f905600b3f3c82f00b70f0cfad2ecd2b
Jhoan Carvajal <jhoans127@outlook.es>
2022-02-03 20:50:13 -0500

    cambie la vista vasada en clase por una funcion para hacer peticiones get y post

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -2,7 +2,7 @@ import imp
 from rest_framework import serializers
 from apps.users.models import User
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import user_api_view
 
 urlpatterns = [
-    path('user/', UserAPIView.as_view(), name = 'user_api')
+    path('user/', user_api_view, name = 'user_api')
 ]
