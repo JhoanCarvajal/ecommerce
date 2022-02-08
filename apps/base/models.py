87044ef4a65492ab927f708b46e6b48eb7a32485
Jhoan Carvajal <jhoans127@outlook.es>
2022-02-07 20:11:38 -0500

    creacion de app base y app product con sus modelos

---
+++ apps/base/models.py
@@ -0,0 +1,17 @@
+from pyexpat import model
+from urllib.parse import DefragResult
+from django.db import models
+
+# Create your models here.
+class BaseModel(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    state = models.BooleanField('Estado', default=True)
+    created_date = models.DateField('Fecha de creación', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Fecha de modificación', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Fecha de eliminación', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+        verbose_name = 'Modelo base'
+        verbose_name_plural = 'Modelos base'
