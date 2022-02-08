33cc2bff1a6d1c69ddaca2b5da6ec583c1812197
Jhoan Carvajal <jhoans127@outlook.es>
2022-02-07 19:09:55 -0500

    creacion de serializer para listar y encriptacion de contraseñas

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -3,14 +3,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, UserListSerializer
 
 @api_view(['GET','POST'])
 def user_api_view(request):
 
     if request.method == 'GET':
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many = True)
+        users = User.objects.all().values('id', 'name', 'email', 'password')
+        users_serializer = UserListSerializer(users, many = True)
         return Response(users_serializer.data, status = status.HTTP_200_OK)
 
     elif request.method == 'POST':
