936b5a5aa21213eb5104212712fb0f942944d733
Jhoan Carvajal <jhoans127@outlook.es>
2022-02-15 20:02:47 -0500

    create api view de productos

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from apps.products.models import *
 
 # Register your models here.
-admin.site.register(MeasureUnit)
-admin.site.register(CategoryProduct)
+class MeasureUnitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description')
+
+
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description')
+
+
+admin.site.register(MeasureUnit, MeasureUnitAdmin)
+admin.site.register(CategoryProduct, CategoryProductAdmin)
 admin.site.register(Indicator)
 admin.site.register(Product)
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,7 +1,19 @@
-from itertools import product
+from rest_framework import generics, status
+from rest_framework.response import Response
 from apps.base.api import GeneralListAPIView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
 
 class ProductListAPIView(GeneralListAPIView):
     serializer_class = ProductSerializer
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Producto creado correctamente!'}, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
