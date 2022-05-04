7f11e05e8a811ec769ea322a96184e08e723444a
Jhoan Carvajal <jhoans127@outlook.es>
2022-05-03 20:07:26 -0500

    detalles y eliminacon logica del producto

--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -17,3 +17,25 @@ class ProductCreateAPIView(generics.CreateAPIView):
             serializer.save()
             return Response({'message':'Producto creado correctamente!'}, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class ProductRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state = True)
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state = True)
+    
+    def delete(self, request, pk=None):
+        product = self.get_queryset().filter(id=pk).first()
+
+        if product:
+            product.state = False
+            product.save()
+            return Response({'message':'Producto eliminado correctamente!'}, status = status.HTTP_200_OK)
+        return Response({'error':'No existe un producto con estos datos!'}, status = status.HTTP_400_BAD_REQUEST)
+
