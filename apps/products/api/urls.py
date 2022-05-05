35ff44881378b5f9c8117fb4497c88ea1613ebe6
Jhoan Carvajal <jhoans127@outlook.es>
2022-05-04 23:30:42 -0500

    update api view

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView, CategoryProductListAPIView, IndicatorListAPIView
-from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView
+from apps.products.api.views.product_views import (ProductListAPIView, ProductCreateAPIView, 
+ProductRetrieveAPIView, ProductDestroyAPIView, ProductUpdateAPIView)
 
 urlpatterns = [
     path('measure_unit/', MeasureUnitListAPIView.as_view(), name='measure_unit'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('product/list/', ProductListAPIView.as_view(), name='product_list'),
     path('product/create/', ProductCreateAPIView.as_view(), name='product_create'),
     path('product/retrieve/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product_retrieve'),
-    path('product/destroy/<int:pk>/', ProductDestroyAPIView.as_view(), name='product_destroy')
+    path('product/destroy/<int:pk>/', ProductDestroyAPIView.as_view(), name='product_destroy'),
+    path('product/update/<int:pk>/', ProductUpdateAPIView.as_view(), name='product_update')
 ]
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import generics, status
 from rest_framework.response import Response
 from apps.base.api import GeneralListAPIView
@@ -39,3 +40,22 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
             return Response({'message':'Producto eliminado correctamente!'}, status = status.HTTP_200_OK)
         return Response({'error':'No existe un producto con estos datos!'}, status = status.HTTP_400_BAD_REQUEST)
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self, pk):
+        return self.get_serializer().Meta.model.objects.filter(state = True).filter(id=pk).first()
+
+    def patch(self, request, pk=None):
+        if self.get_queryset(pk):
+            product_serializer = self.serializer_class(self.get_queryset(pk))
+            return Response(product_serializer.data, status = status.HTTP_200_OK)
+        return Response({'error':'No existe un producto con estos datos!'}, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        if self.get_queryset(pk):
+            product_serializer = self.serializer_class(self.get_queryset(pk), data = request.data)
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status = status.HTTP_200_OK)
+            return Response(product_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
