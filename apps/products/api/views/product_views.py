c9a27681816c6501d5c4bccab86601631bc5eac8
Jhoan Carvajal <jhoans127@outlook.es>
2022-06-03 22:01:53 -0500

    ListCreate y RetrieveUpdateDestroy

--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,16 +1,12 @@
 import re
 from rest_framework import generics, status
 from rest_framework.response import Response
-from apps.base.api import GeneralListAPIView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
 
-class ProductListAPIView(GeneralListAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
-
-
-class ProductCreateAPIView(generics.CreateAPIView):
-    serializer_class = ProductSerializer
+    queryset = ProductSerializer.Meta.model.objects.filter(state = True)
 
     def post(self, request):
         serializer = self.serializer_class(data = request.data)
@@ -19,33 +15,15 @@ class ProductCreateAPIView(generics.CreateAPIView):
             return Response({'message':'Producto creado correctamente!'}, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
-
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state = True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(state = True).first()
     
-    def delete(self, request, pk=None):
-        product = self.get_queryset().filter(id=pk).first()
-
-        if product:
-            product.state = False
-            product.save()
-            return Response({'message':'Producto eliminado correctamente!'}, status = status.HTTP_200_OK)
-        return Response({'error':'No existe un producto con estos datos!'}, status = status.HTTP_400_BAD_REQUEST)
-
-class ProductUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self, pk):
-        return self.get_serializer().Meta.model.objects.filter(state = True).filter(id=pk).first()
-
     def patch(self, request, pk=None):
         if self.get_queryset(pk):
             product_serializer = self.serializer_class(self.get_queryset(pk))
@@ -59,3 +37,12 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
                 product_serializer.save()
                 return Response(product_serializer.data, status = status.HTTP_200_OK)
             return Response(product_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        product = self.get_queryset().filter(id=pk).first()
+
+        if product:
+            product.state = False
+            product.save()
+            return Response({'message':'Producto eliminado correctamente!'}, status = status.HTTP_200_OK)
+        return Response({'error':'No existe un producto con estos datos!'}, status = status.HTTP_400_BAD_REQUEST)
